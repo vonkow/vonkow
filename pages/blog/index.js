216dbbf8849cf8cf62e7e11794523c69d01324be
Caz <caz@vonkow.com>
2021-09-15 23:13:34 -0700

    Link fix2

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Link from 'next/Link'
+import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 import fs from 'fs';
 import path from 'path';
